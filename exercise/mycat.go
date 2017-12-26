9ee24b0e64c97e391632b2291c3d7bf403121aed
ss <123456@qq.com>
2017-12-26 17:39:38 +0800

    NEW!

---
+++ exercise/mycat.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func printFile(name string) {
+	buf, err := ioutil.ReadFile(name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(buf))
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		os.Exit(1)
+	}
+	printFile(os.Args[1])
+
+}
